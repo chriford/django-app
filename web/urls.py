2250cf45befa5f983792ea65ba5c0e71d1e67cbf
chriford <66867121+chriford@users.noreply.github.com>
2020-06-13 09:37:14 -0700

    git commit

--- web/urls.py
+++ web/urls.py
@@ -4,4 +4,5 @@ from web import views
 urlpatterns = [
     path('', views.index_view, name='index-page'),
     path('about/', views.about_view, name='about-page')
+    path('posts/', views.PublicPosts, name='basicposts')
 ]
